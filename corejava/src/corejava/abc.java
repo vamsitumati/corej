6cd5fdd2e23edb770e7cb95d60258948a60e1e0f
Vamsi Tumati <vamsitumati@yahoo.com>
2016-11-01 15:25:17 -0400

    abc

---
+++ corejava/src/corejava/abc.java
@@ -0,0 +1,12 @@
+package corejava;
+
+import junit.framework.TestCase;
+import org.junit.*;
+
+public class abc extends TestCase {
+
+	@Test
+	public void testMeth() {
+		fail("Not yet implemented");
+	}
+}
